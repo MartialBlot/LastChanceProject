d0cabecec463c35c3341fbe149e7db68b960ad1b
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-06-26 14:37:29 +0200

    Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NesComponent } from './nes/nes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NesComponent
   ],
   imports: [
     BrowserModule,
