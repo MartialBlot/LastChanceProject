82d3a42871a60f7f23300537fcdfbc6c070b1d91
Jérémy Foucault <jeremfouc@orange.fr>
2019-06-26 15:58:04 +0200

    avant merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { VideoComponent } from './video/video.component';
+import { ChoiceComponent } from './choice/choice.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'play', component: VideoComponent}
+  { path: 'play', component: VideoComponent},
+  { path: 'choix', component: ChoiceComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NesComponent } from './nes/nes.component';
 import { VideoComponent } from './video/video.component';
+import { ChoiceComponent } from './choice/choice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NesComponent,
-    VideoComponent
+    VideoComponent,
+    ChoiceComponent
   ],
   imports: [
     BrowserModule,
