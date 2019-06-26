f8a3577fdc8343d8f5ecadab6409ae77c5863fda
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-06-26 15:26:56 +0200

    Space racing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NesComponent } from './nes/nes.component';
 import { FirstPlanetComponent } from './first-planet/first-planet.component';
+import { SpaceRacingComponent } from './space-racing/space-racing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NesComponent,
-    FirstPlanetComponent
+    FirstPlanetComponent,
+    SpaceRacingComponent,
   ],
   imports: [
     BrowserModule,
