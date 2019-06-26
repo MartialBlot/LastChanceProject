7233ede795299f282674b6f67eb7dfded5bd9268
BLOT Martial <martial.blot@outlook.fr>
2019-06-27 00:13:05 +0200

    Controles diagonales OK

--- src/app/first-planet/first-planet.component.ts
+++ src/app/first-planet/first-planet.component.ts
@@ -82,24 +82,52 @@ export class FirstPlanetComponent implements AfterViewInit {
     function gameLoop() {
       //Control droit
       if (keyState[39] || keyState[68]){
-          pX+=3;
+          bX+=3;
+          x-=3;
           droit();
       }
       //Control gauche
       if (keyState[37] || keyState[65]){
-        pX-=3;
+        bX-=3;
+        x+=3;
         gauche();
       }
       //Control haut
       if (keyState[38] || keyState[87]){
         haut();
-        pY-=3;
+        bY-=3;
+        y+=3;
       }
       //Control bas
       if (keyState[40] || keyState[83]){
         bas();
-        pY+=3;
+        bY+=3;
+        y-=3;
       }
+      //diagoDroit
+      if ((keyState[38] || keyState[87]) && (keyState[39] || keyState[68])){
+        diagoDroit();
+        bY-=3;
+        y+=3;
+      }
+      //diagoGauche
+      if ((keyState[38] || keyState[87]) && (keyState[37] || keyState[65])){
+        diagoGauche();
+        bY-=3;
+        y+=3;
+      }
+    //diagoBasDroit
+    if ((keyState[40] || keyState[83]) && (keyState[39] || keyState[68])){
+      diagoBasDroit();
+      bY+=3;
+      y-=3;
+    }
+    //diagoBasGauche
+    if ((keyState[40] || keyState[83]) && (keyState[37] || keyState[65])){
+      diagoBasGauche();
+      bY+=3;
+      y-=3;
+    }
 
       setTimeout(gameLoop, 10);
     }
@@ -146,6 +174,34 @@ export class FirstPlanetComponent implements AfterViewInit {
       pFrameCount = 8;
       pSrcY= 0;
       }
+
+    function diagoDroit(){
+      playerWidth = 800;
+      pCols = 8;
+      pFrameCount = 8;
+      pSrcY= 700;
+    }
+
+    function diagoGauche(){
+      playerWidth = 800;
+      pCols = 8;
+      pFrameCount = 8;
+      pSrcY= 500;
+    }
+
+    function diagoBasDroit(){
+      playerWidth = 800;
+      pCols = 8;
+      pFrameCount = 8;
+      pSrcY= 600;
+    }
+
+    function diagoBasGauche(){
+      playerWidth = 800;
+      pCols = 8;
+      pFrameCount = 8;
+      pSrcY= 400;
+    }
 
     function draw(){
       updateFrame();
