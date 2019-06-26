f20253b0ba7a77cad12f6f313d9bb1b220834e87
Jérémy Foucault <jeremfouc@orange.fr>
2019-06-26 15:25:07 +0200

    ajout video redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VideoComponent } from './video/video.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'play', component: VideoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NesComponent } from './nes/nes.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NesComponent
+    NesComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
