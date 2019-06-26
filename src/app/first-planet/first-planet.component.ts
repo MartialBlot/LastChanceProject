8d6ddbc7275b10b9925e5ffdf079d6ad2b7d7519
BLOT Martial <martial.blot@outlook.fr>
2019-06-26 17:47:17 +0200

    map + position initiale vaisseau niveau 1

--- src/app/first-planet/first-planet.component.ts
+++ src/app/first-planet/first-planet.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { by } from 'protractor';
 
 @Component({
   selector: 'app-first-planet',
@@ -14,16 +15,16 @@ export class FirstPlanetComponent implements AfterViewInit {
     let ctx = canvas.getContext("2d");
 
     let background = new Image();
-    background.src = "assets/images/map.png";
-    let bX = 0;
-    let bY = 1080;
+    background.src = "assets/images/maptest.png";
+    let bX = 300;
+    let bY = 300;
 
     let widowMaker = new Image();
     widowMaker.src = "assets/images/widowMaker1.png"
 
     let canvasWidth = 1200;
     let canvasHeight = 800;
-
+// Sprite du vaisseau
     let spriteWidth = 2370;
     let spriteHeight = 1030;
     let rows = 1;
@@ -32,11 +33,12 @@ export class FirstPlanetComponent implements AfterViewInit {
     let height = spriteHeight/rows;
     let curFrame = 0;
     let frameCount = 3;
-    let x=0;
-    let y=0;
+    let x=400;
+    let y=320;
     let srcX= 0;
     let srcY= 0;
-    canvas.width = canvasWidth;
+
+    canvas.width =  canvasWidth;
     canvas.height = canvasHeight;
 
     function updateFrame(){
@@ -47,7 +49,7 @@ export class FirstPlanetComponent implements AfterViewInit {
 
     function draw(){
       updateFrame();
-      ctx.drawImage(background,bX,0,1400,bY,0,-100,1200,900);
+      ctx.drawImage(background,bX,bY,1400,bY,0,-100,1200,900);
       ctx.drawImage(widowMaker,srcX,srcY,width,height,x,y,80,120);
     }
     setInterval(draw,80);
